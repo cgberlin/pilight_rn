856f17a53ec33f420e2c0e1bf7d04ff94fa6aabc
Cody Berlin <codyberlin@gmail.com>
2019-06-13 02:29:32 -0700

    app fully functional:

--- App.js
+++ App.js
@@ -3,28 +3,38 @@ import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import firebase from 'react-native-firebase'
 import Picker from './components/Picker'
 import { fromHsv } from 'react-native-color-picker'
+import { Emitter } from 'react-native-particles' // lol
+import TimePicker from "react-native-24h-timepicker" // im lazy
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.stateRef = firebase.firestore().collection('states');
     this.userRef = firebase.firestore().collection('user');
+    this.displayRef = firebase.firestore().collection('display');
     this.unsubStates = null;
     this.unsubUser = null;
+    this.unsubDisplay = null;
     this.state = {
       type: '',
       color: {r:0,g:0,b:0},
-      lastColorUpdate: new Date()
-    };
+      lastColorUpdate: new Date(),
+      pattern: '',
+      startTime: '',
+      stopTime: ''
+    }
   }
 
   async componentDidMount() {
     this.unsubStates = this.stateRef.onSnapshot(this._onStateRefUpdate.bind(this))
     this.unsubUser = this.userRef.onSnapshot(this._onUserRefUpdate.bind(this))
+    this.unsubDisplay = this.displayRef.onSnapshot(this._onDisplayRefUpdate.bind(this))
   }
 
   componentWillUnmount() {
-    this.unsubStates();
+    this.unsubStates()
+    this.unsubUser()
+    this.unsubDisplay()
   }
 
   secondsBetweenDates(dateOne, dateTwo) {
@@ -46,6 +56,11 @@ export default class App extends React.Component {
     else return styles.buttonWrapper
   }
 
+  _getPatternButtonStyle(type) {
+    if (type === this.state.pattern) return styles.activePatternButtonWrapper
+    else return styles.patternButtonWrapper
+  }
+
   _colorUpdated(color) {
     color = this.hexToRgb(fromHsv(color))
     let secondsBetween = this.secondsBetweenDates(this.state.lastColorUpdate, new Date())
@@ -61,8 +76,38 @@ export default class App extends React.Component {
     }
   }
 
+  _patternUpdated(pattern) {
+    this.displayRef.doc('config').update({
+      pattern
+    })
+  }
+
   _stateUpdated(type) {
     this.stateRef.doc('control_type').update({type})
+  }
+
+  onTimeCancel() {
+    this.StartTimePicker.close();
+    this.StopTimePicker.close();
+  }
+
+  onTimeConfirm(hour, minute, type) {
+    if (type === 'start') {
+      let startTime = `${hour}:${minute}` 
+      this.displayRef.doc('config').update({
+        startTime
+      })
+      this.setState({ startTime })
+      this.StartTimePicker.close()
+    } 
+    if (type === 'stop') {
+      let stopTime = `${hour}:${minute}` 
+      this.displayRef.doc('config').update({
+        stopTime
+      })
+      this.setState({ stopTime });
+      this.StopTimePicker.close()
+    }
   }
 
   _onStateRefUpdate(querySnapshot) {
@@ -79,6 +124,17 @@ export default class App extends React.Component {
       let { color } = doc.data()
       this.setState({
         color
+      })
+    })
+  }
+
+  _onDisplayRefUpdate(querySnapshot) {
+    querySnapshot.forEach(doc => {
+      let { startTime, stopTime, pattern } = doc.data()
+      this.setState({
+        pattern,
+        startTime, 
+        stopTime
       })
     })
   }
@@ -106,13 +162,109 @@ export default class App extends React.Component {
             <Text style={styles.buttonText}>Party</Text>
           </TouchableOpacity>
         </View>
+        <View style={styles.container}>
+          <TouchableOpacity 
+              style={this._getPatternButtonStyle('SOLID')}
+              onPress={() => this._patternUpdated('SOLID')}
+            >
+              <Text style={styles.buttonText}>Solid</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+              style={this._getPatternButtonStyle('FLASH')}
+              onPress={() => this._patternUpdated('FLASH')}
+            >
+              <Text style={styles.buttonText}>Flash</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+              style={this._getPatternButtonStyle('BREATHE')}
+              onPress={() => this._patternUpdated('BREATHE')}
+            >
+              <Text style={styles.buttonText}>Breathe</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+              style={this._getPatternButtonStyle('OFF')}
+              onPress={() => this._patternUpdated('OFF')}
+            >
+              <Text style={styles.buttonText}>Off</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.timeContainer}>
+            <Text style={styles.timeText}>Start: {this.state.startTime}</Text>
+            <TouchableOpacity 
+              style={styles.activeButtonWrapper}
+              onPress={() => this.StartTimePicker.open()}
+            >
+              <Text style={styles.buttonText}>Set</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.timeContainer}>
+            <Text style={styles.timeText}>Stop: {this.state.stopTime}</Text>
+            <TouchableOpacity 
+                style={styles.activeButtonWrapper}
+                onPress={() => this.StopTimePicker.open()}
+              >
+                <Text style={styles.buttonText}>Set</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
         {this.state.type === 'USER' &&
             <Picker pickedColor={(color) => this._colorUpdated(color)}/>
         }
         {this.state.type === 'PARTY' &&
             <Image style={styles.partyImage} source={{uri: 'https://i.imgur.com/2m2Y7pQ.gif'}} />
         }
-       
+        <Emitter
+          numberOfParticles={50}
+          emissionRate={5}
+          interval={200}
+          particleLife={3000}
+          direction={90}
+          spread={360}
+          gravity={10}
+          fromPosition={{ x: 180, y: 0 }}
+        >
+          <Image source={require('./assets/cat.png')} />
+        </Emitter>
+        <Emitter
+          numberOfParticles={50}
+          emissionRate={5}
+          interval={200}
+          particleLife={3000}
+          direction={90}
+          spread={360}
+          gravity={10}
+          fromPosition={{ x: 70, y: 0 }}
+        >
+          <Image source={require('./assets/cat.png')} />
+        </Emitter>
+        <Emitter
+          numberOfParticles={50}
+          emissionRate={5}
+          interval={200}
+          particleLife={3000}
+          direction={90}
+          spread={360}
+          gravity={10}
+          fromPosition={{ x: 250, y: 0 }}
+        >
+          <Image source={require('./assets/cat.png')} />
+        </Emitter>
+        <TimePicker
+          ref={ref => {
+            this.StartTimePicker = ref;
+          }}
+          onCancel={() => this.onTimeCancel()}
+          onConfirm={(hour, minute) => this.onTimeConfirm(hour, minute, 'start')}
+        />
+        <TimePicker
+          ref={ref => {
+            this.StopTimePicker = ref;
+          }}
+          onCancel={() => this.onTimeCancel()}
+          onConfirm={(hour, minute) => this.onTimeConfirm(hour, minute, 'stop')}
+        />
       </View>
     );
   }
@@ -134,6 +286,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginTop: 20
   },
+  timeContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    height: 100
+  },
   buttonWrapper: {
     width: '30%',
     height: 40,
@@ -149,6 +308,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5
+  },
+  patternButtonWrapper: {
+    flex: 1,
+    height: 40,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  activePatternButtonWrapper: {
+    flex: 1,
+    height: 40,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   buttonText: {
     color: 'white',
